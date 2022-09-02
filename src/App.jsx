7fa00f1c014c9b02d6ba95d32e8c9cf9f00847a7
devfrank9 <devfrank9@gmail.com>
2022-09-02 19:05:04 +0900

    ✨ Setting : vscode 전용 셋팅 추가, 파일 경로 단순화 위한 파일 이름변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
-import RootRoute from './routes/route';
+import RootRoute from './routes';
 import GlobalStyle from './styles/GlobalStyle';
-import theme from './theme/theme';
+import theme from './theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <RootRoute />
-      <GlobalStyle />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <RootRoute />
+        <GlobalStyle />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
