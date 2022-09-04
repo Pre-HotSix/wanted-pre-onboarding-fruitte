f7c9df1fdedb76c5e207ffb065c7727cee3ccdaf
sasumpi123 <sasumpi123@naver.com>
2022-09-04 17:20:31 +0900

    ✨ Feat: 관리자 상품등록 페이지 기능구현(css제외)

---
+++ src/components/Form/AdminEnrollForm.js
@@ -0,0 +1,143 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import NumberFormat from 'react-number-format';
+
+const AdminEnrollForm = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [salePrice, setSalePrice] = useState('');
+  const [qty, setQty] = useState('');
+  const [shippingFee, setShippingFee] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [options, setOptions] = useState([]);
+  const [desc, setDesc] = useState('');
+
+  const [showOptionInput, setShowOptionInput] = useState(false);
+  const [tempOption, setTempOption] = useState('');
+
+  const uploadImg = () => {
+    setImgUrl('https://cdn.imweb.me/thumbnail/20220521/69f390486a3e3.png');
+  };
+  const regProduct = () => {
+    navigate('/adminmanage');
+  };
+  const handleAddOption = () => {
+    setOptions([...options, tempOption]);
+    setShowOptionInput(false);
+    setTempOption('');
+  };
+  const hideOptionInput = () => {
+    setShowOptionInput(false);
+    setTempOption('');
+  };
+
+  return (
+    <div>
+      <label htmlFor="productName">상품명</label>
+      <input
+        id="productName"
+        type="text"
+        value={name}
+        onChange={e => setName(e.target.value)}
+        maxLength="100"
+      />
+      <label htmlFor="productName">{`${name.length} / 100`}</label>
+      <br />
+      <label htmlFor="price">판매가</label>
+      <NumberFormat
+        id="price"
+        thousandSeparator={true}
+        prefix={'₩'}
+        allowNegative={false}
+        onValueChange={value => {
+          setPrice(value.value);
+        }}
+      />
+      <br />
+      <label htmlFor="salePrice">할인가</label>
+      <NumberFormat
+        id="salePrice"
+        thousandSeparator={true}
+        prefix={'₩'}
+        allowNegative={false}
+        onValueChange={value => {
+          setSalePrice(value.value);
+        }}
+      />
+      <br />
+      <label htmlFor="qty">수량</label>
+      <NumberFormat
+        id="qty"
+        thousandSeparator={true}
+        suffix={'개'}
+        allowNegative={false}
+        onValueChange={value => {
+          setQty(value.value);
+        }}
+      />
+      <br />
+      <label htmlFor="shippingFee">배송비</label>
+      <NumberFormat
+        id="shippingFee"
+        thousandSeparator={true}
+        prefix={'₩'}
+        allowNegative={false}
+        onValueChange={value => {
+          setShippingFee(value.value);
+        }}
+      />
+      <br />
+      <label htmlFor="shippingFee">옵션</label>
+      <input
+        type="button"
+        value="옵션 등록"
+        onClick={() => {
+          setShowOptionInput(true);
+        }}
+      />
+      <input
+        type="button"
+        value="옵션 초기화"
+        onClick={() => {
+          setOptions([]);
+        }}
+      />
+      {showOptionInput && (
+        <div>
+          <input
+            type="text"
+            value={tempOption}
+            onChange={e => {
+              setTempOption(e.target.value);
+            }}
+          />
+          <input type="button" value="추가" onClick={handleAddOption} />
+          <input type="button" value="취소" onClick={hideOptionInput} />
+        </div>
+      )}
+      {options.length > 0 &&
+        options.map((option, idx) => {
+          return <p key={idx}>{option}</p>;
+        })}
+      <br />
+      <label>메인이미지</label>
+      <input type="button" onClick={uploadImg} value="이미지 등록" />
+      {imgUrl !== '' && <img src={imgUrl} />}
+      <br />
+      <textarea
+        id="desc"
+        value={desc}
+        onChange={e => setDesc(e.target.value)}
+        maxLength="1500"
+      />
+      <label htmlFor="name">{`${desc.length} / 1500`}</label>
+      <br />
+      <input type="button" onClick={regProduct} value="상품 등록" />
+    </div>
+  );
+};
+
+export default AdminEnrollForm;
--- src/pages/AdminEnroll/AdminEnroll.jsx
+++ src/pages/AdminEnroll/AdminEnroll.jsx
@@ -1,7 +1,141 @@
+import styled from 'styled-components';
 import React from 'react';
+import AdminEnrollForm from 'components/Form/AdminEnrollForm';
+import { AiOutlineHome } from 'react-icons/ai';
 
 const AdminEnroll = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Navigation>
+        <Logo src="/images/logo.jpeg" />
+        <ul>
+          <li className="list active">
+            <HomeIcon />
+            <span className="title">Home</span>
+          </li>
+        </ul>
+      </Navigation>
+      <Article>
+        <div className="topWrap">
+          <h2 className="title">상품 등록</h2>
+        </div>
+        <Line />
+        <AdminEnrollForm />
+      </Article>
+    </Container>
+  );
 };
 
+const Container = styled.section`
+  width: 1200px;
+  height: 90vh;
+  padding: 20px;
+  margin: 0 auto;
+  background: #2a750e;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const Logo = styled.img`
+  display: block;
+  width: 164px;
+  height: 45px;
+  margin-top: 15px;
+  margin-bottom: 45px;
+`;
+const Navigation = styled.nav`
+  position: relative;
+  width: 216px;
+  height: 100%;
+  background: #2a750e;
+  border-left: 5px solid #2a750e;
+  overflow-x: hidden;
+  ul {
+    width: 100%;
+    padding-left: 5px;
+    margin: 0;
+    li {
+      position: relative;
+      width: 100%;
+      height: 45px;
+      margin-bottom: 10px;
+      display: flex;
+      align-items: center;
+      color: #fff;
+      padding-left: 15px;
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;
+      &.active {
+        background: #fff;
+        color: #4c9c2e;
+        &::before {
+          content: '';
+          position: absolute;
+          top: -40px;
+          right: 0;
+          width: 40px;
+          height: 40px;
+          background: #2a750e;
+          border-radius: 45%;
+          box-shadow: 15px 15px 0 #fff;
+        }
+        &::after {
+          content: '';
+          position: absolute;
+          bottom: -40px;
+          right: 0;
+          width: 40px;
+          height: 40px;
+          background: #2a750e;
+          border-radius: 45%;
+          box-shadow: 15px -15px 0 #fff;
+        }
+      }
+      .title {
+        position: relative;
+        display: block;
+        padding-left: 8px;
+        white-space: nowrap;
+      }
+    }
+  }
+`;
+const HomeIcon = styled(AiOutlineHome)`
+  width: 20px;
+  height: 20px;
+`;
+
+const Article = styled.article`
+  background-color: #fff;
+  padding: 30px;
+  border-radius: 20px;
+  width: 100%;
+  height: 100%;
+
+  .topWrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .title {
+      margin: 0;
+    }
+    > button {
+      width: 100px;
+      height: 40px;
+      border-radius: 20px;
+      background-color: #4c9c2e;
+      a {
+        text-decoration: none;
+        color: #fff;
+      }
+    }
+  }
+`;
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #a9a9a9;
+  margin: 20px 0;
+`;
 export default AdminEnroll;
