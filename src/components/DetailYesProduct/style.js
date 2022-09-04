9489854c51d974079342faf7d2a98c09754b03a2
MINYUKYUNG <minew0223@gmail.com>
2022-09-05 01:24:52 +0900

    💄 Style : 스타일 파일 분리

---
+++ src/components/DetailYesProduct/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const YesContainer = styled.div`
+  position: absolute;
+  padding: 50px 120px;
+  width: 1440px;
+  height: 1302px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
---
+++ src/pages/ProductDetail/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const ProductLayout = styled.div`
+  padding-top: 90px;
+  position: relative;
+`;
