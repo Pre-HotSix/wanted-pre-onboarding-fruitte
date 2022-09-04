c3339977ddf4de9290d45c6a0c345437c076b9b2
MINYUKYUNG <minew0223@gmail.com>
2022-09-04 16:58:17 +0900

    ⚡️ Feature : 상품 상세 페이지 생성

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import YesProduct from '../../components/DetailYesProduct/YesProduct';
+import { dummies } from 'constants/dummy';
 
 const ProductDetail = () => {
-  return <div></div>;
+  const [list] = useState(dummies);
+  const { docId } = useParams();
+
+  const result = () => {
+    if (docId) {
+      const numId = Number(docId);
+      if (numId <= list.length && docId[0] !== '-' && docId[0] !== '0') {
+        const info = { numId, list };
+        return <YesProduct {...info} />;
+      }
+    }
+  };
+
+  return <ProductLayout>{result()}</ProductLayout>;
 };
 
 export default ProductDetail;
+
+const ProductLayout = styled.div`
+  padding-top: 90px;
+  position: relative;
+`;
