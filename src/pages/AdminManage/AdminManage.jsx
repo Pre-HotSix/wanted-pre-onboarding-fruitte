fbaf6dbe748e56357c0f246a3f0f8345bb7b4000
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2022-09-04 23:20:14 +0900

    :recycle: Refactor: [#7] 관리자 상품 목록 관리 페이지 코드 리팩토링 및 디자인 수정
    사용하지 않는 코드 삭제, 필요없는 태그 삭제 및 container background-color 수정

--- src/pages/AdminManage/AdminManage.jsx
+++ src/pages/AdminManage/AdminManage.jsx
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import * as S from './AdminManage.styles';
-import { Navigation, ProductItem } from 'components';
+import * as S from './style';
+import { ProductItem } from 'components';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { productState } from 'recoil/product';
 
@@ -23,49 +24,46 @@ export default function AdminManage() {
   };
 
   return (
-    <>
-      <Navigation />
-      <S.Container>
-        <S.Sidebar>
-          <S.Logo src="/images/logo.jpeg" />
-          <ul>
-            <li className="list active">
-              <S.HomeIcon />
-              <span className="title">Home</span>
-            </li>
-          </ul>
-        </S.Sidebar>
-        <S.Article>
-          <div className="topWrap">
-            <h2 className="title">STORE LIST</h2>
-            <button>
-              <Link to="/adminenroll">+ADD</Link>
-            </button>
+    <S.Container>
+      <S.Sidebar>
+        <S.Logo src="/images/logo.png" />
+        <ul>
+          <li className="list active">
+            <S.HomeIcon />
+            <span className="title">Home</span>
+          </li>
+        </ul>
+      </S.Sidebar>
+      <S.Article>
+        <div className="topWrap">
+          <h2 className="title">STORE LIST</h2>
+          <button>
+            <Link to="/adminenroll">+ADD</Link>
+          </button>
+        </div>
+        <S.Line />
+        <div className="table">
+          <div className="row">
+            <span>상품번호</span>
+            <span>제품정보</span>
+            <span>제품가격</span>
+            <span>제품수량</span>
+            <span>제품 노출 여부</span>
+            <span>수정</span>
+            <span>삭제</span>
           </div>
-          <S.Line />
-          <div className="table">
-            <div className="row">
-              <span>상품번호</span>
-              <span>제품정보</span>
-              <span>제품가격</span>
-              <span>제품수량</span>
-              <span>제품 노출 여부</span>
-              <span>수정</span>
-              <span>삭제</span>
-            </div>
-            <div className="column">
-              {product.map((el, index) => (
-                <ProductItem
-                  key={el.id}
-                  item={el}
-                  index={index}
-                  onClickDelete={onClickDelete}
-                />
-              ))}
-            </div>
+          <div className="column">
+            {product.map((el, index) => (
+              <ProductItem
+                key={el.id}
+                item={el}
+                index={index}
+                onClickDelete={onClickDelete}
+              />
+            ))}
           </div>
-        </S.Article>
-      </S.Container>
-    </>
+        </div>
+      </S.Article>
+    </S.Container>
   );
 }
