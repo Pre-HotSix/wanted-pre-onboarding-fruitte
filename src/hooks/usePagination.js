0da1171afe99fd3da91a12894c3df9ab1185aede
Jongho Lee <90244663+devfrank9@users.noreply.github.com>
2022-09-05 02:43:21 +0900

    Revert "Commoms: Layout 폴더 구조 변경 및 route Layout 적용"

--- src/hooks/usePagination.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export const usePagination = () => {
-  const [page, setPage] = useState(1);
-  const limit = 10;
-  const offset = (page - 1) * limit;
-
-  return { page, limit, offset, setPage };
-};
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,10 +1,4 @@
-const colors = {
-  MAIN_COLOR: '#4C9C2E',
-  BORDER_COLOR: '#E9E9E9',
-  PRODUCT_BORDER_BOTTOM: '#2C3131',
-  WHITE: '#FFFFFF',
-  LIGHT_GRAY: '#A9A9A9',
-};
+const colors = {};
 const mediaScreen = {};
 const fontSize = {};
 
